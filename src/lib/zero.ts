37873958b5792b4fe98f99ea430043e15a0fb122
ldelivery <108382514+ldelivery@users.noreply.github.com>
2025-05-18 07:22:50 +0200

    Updated zero.ts

--- src/lib/zero.ts
+++ src/lib/zero.ts
@@ -40,8 +40,6 @@ export const zero = new Zero<Schema>({
   userID: 'default-user',
   auth: () => import.meta.env.ZERO_AUTH_SECRET,
   server: "https://skillsafari-zero-cache.fly.dev",
-  serverName: "zero-cache-server",
-  daf, "aksdf",
   schema,
   kvStore: "idb",
 });
