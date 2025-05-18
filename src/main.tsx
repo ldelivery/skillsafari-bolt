5e91abbd59535c14447c69e2c0ee84fe06a4a041
ldelivery <108382514+ldelivery@users.noreply.github.com>
2025-05-18 07:18:28 +0200

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { ZeroProvider } from "@rocicorp/zero";
+import { ZeroProvider } from "@rocicorp/zero/react";
 import { zero } from './lib/zero';
 
 createRoot(document.getElementById('root')!).render(
